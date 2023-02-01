8930c6ab9b86ee86e718c317b80ece2954269346
Dianact <dianact@arquitecsoft.com>
2023-01-31 23:02:54 -0500

    Cambios Backend

---
+++ src/main/java/com/mitocode/model/VitalSign.java
@@ -0,0 +1,39 @@
+package com.mitocode.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+
+public class VitalSign {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer idVitalSign;
+
+    @ManyToOne
+    @JoinColumn(name = "id_patient", nullable = false, foreignKey = @ForeignKey(name= "FK_VITALSIGN_PATIENT"))
+    private Patient patient; 
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @Column(nullable = false, length = 500)
+    private String temperature;
+
+    @Column(nullable = false, length = 500)
+    private String heartbeat;
+
+    @Column(nullable = false, length = 500)
+    private String respiratoryRate;
+
+}
